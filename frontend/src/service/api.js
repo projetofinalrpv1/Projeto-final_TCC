9e437e8ec87fcfafc5b4f8e7dda254d1d7d411f8
David Moreto <moretodavid355@gmail.com>
2026-03-30 12:42:37 -0300

    Atualizando variável de ambiente

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3333',
+  baseURL: import.meta.env.VITE_API_URL ,
 });
 
 // Interceptor de REQUEST: injeta o token em toda requisição
