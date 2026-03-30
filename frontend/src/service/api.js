db2c87cefac63bfc4b3e337bdc6839e7c64ad787
David Moreto <moretodavid355@gmail.com>
2026-03-30 12:47:02 -0300

    teste

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,6 +1,8 @@
 // src/service/api.js
 import axios from 'axios';
 
+console.log('Link da API:', import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ,
 });
